eb61f3ca7811d75b2681984730ad6f8c792b3cc4
Sebastian <sebastian.jackson62@gmail.com>
2019-03-12 22:04:53 +1100

    BP4 - No testing

--- NeuralNetwork.js
+++ NeuralNetwork.js
@@ -154,7 +154,7 @@ function neuron(id,layer,network) {
 
 function network_matrix(id, detailsArray) {
 	this.id = id
-	this.layers = [] // this.layers[layer][neuron][[value, activation, activation_d, error],[bias, weight, weight ,weight,...]]
+	this.layers = [] // this.layers[layer][neuron][[value, activation, activation_d, error],[bias, weight, weight , weight,...],[bias_D, weight_D, weight_D, weight_D,...]]
 	
 	for(var i = 0; i < detailsArray.length; i++) { //	Build data structure
 		this.layers.push(new Array(detailsArray[i]))
@@ -162,7 +162,7 @@ function network_matrix(id, detailsArray) {
 	
 	for(var i = 1; i < this.layers.length; i++) { //	Build Data Structure
 		for(var j = 0; j < this.layers[i].length; j++) {
-			this.layers[i][j] = [[0,0,0,0],new Array(this.layers[i-1].length + 1)]
+			this.layers[i][j] = [[0,0,0,0],new Array(this.layers[i-1].length + 1),new Array(this.layers[i-1].length + 1)]
 		}
 	}
 	
@@ -170,8 +170,10 @@ function network_matrix(id, detailsArray) {
 		for(var j = 0; j < this.layers[i].length; j++) {
 			for(var n = 0; n < this.layers[i][j][1].length-1; n++) {
 				this.layers[i][j][1][n+1] = (Math.random()-0.5)*2
+				this.layers[i][j][2][n+1] = 0
 			}
 			this.layers[i][j][1][0] = 0
+			this.layers[i][j][2][0] = 0
 		}
 	}
 	
@@ -207,7 +209,8 @@ function network_matrix(id, detailsArray) {
 		}
 	}
 	
-	this.error = function(corrects) {
+	this.error = function(corrects) {//	Further optimisation can be done here by using variables for current layer.length and next/prev. layer.length ect.
+									 //	Further optimisations can also be storing the value of sigmoid_d(value)
 		
 		this.cost = 0
 		
@@ -218,29 +221,33 @@ function network_matrix(id, detailsArray) {
 		
 		//	-BP1-	Error for last layer Neurons
 		for(var i = 0; i < this.layers[this.layers.length-1].length; i++) {
-			
-		}
-		
-		//	-BP1-	Error for last layer Neurons
-		for(var i = 0; i < this.layer[this.layer.length-1].neuron.length; i++) {
-			this.layer[this.layer.length-1].neuron[i].error = (this.layer[this.layer.length-1].neuron[i].activation - expectedArray[i])*this.deActivate(this.layer[this.layer.length-1].neuron[i].value)
+			this.layers[this.layers.length-1][i][0][3] = (this.layers[this.layers.length-1][i][0][1] - corrects[i])*this.sigmoid_d(this.layers[this.layers.length-1][i][0][0])
 		}
 		
 		//	-BP2-	Error for previous layer Neurons
-		for(var i = this.layer.length-2; i > -1; i--) {
-			for(var j = 0; j < this.layer[i].neuron.length; j++) {
-				this.layer[i].neuron[j].error = 0
-				for(var n = 0; n < this.layer[i+1].neuron.length; n++) {
-					this.layer[i].neuron[j].error += this.layer[i+1].neuron[n].weight[j]*this.layer[i+1].neuron[n].error
+		for(var i = this.layers.length-2; i > -1; i--) {
+			for(var j = 0; j < this.layers[i].length; j++) {
+				this.layers[i][j][0][3] = 0
+				for(var n = 0; n < this.layer[i+1].length; n++) {
+					this.layers[i][j][0][3] += this.layers[i+1][n][1][j+1]*this.layers[i+1][n][0][3]
 				}
-				this.layer[i].neuron[j].error *= this.deActivate(this.layer[i].neuron[j].value)
+				this.layers[i][j][0][3] *= this.sigmoid_d(this.layers[i][j][0][0])
 			}
 		}
 		
 		//	-BP3-	Delta for Biases
-		for(var i = 0; i < this.layer.length-1; i++) {
-			for(var j = 0; j < this.layer[i].neuron.length; j++) {
-				this.layer[i].neuron[j].biasDelta.push(this.layer[i].neuron[j].error)
+		for(var i = 0; i < this.layers.length-1; i++) {
+			for(var j = 0; j < this.layers[i][j].length; j++) {
+				this.layers[i][j][2][0] += this.layers[i][j][0][3]
+			}
+		}
+		
+		//	-BP4-	Delta for Weights
+		for(var i = 0; i < this.layer.length; i++) {
+			for(var j = 0; j < this.layer[i][j].length; j++) {
+				for(var n = 0; n < this.layers[i][j][1].length-1; n++) {
+					this.layers[i][j][2][n+1] += this.layers[i-1][n][0][1]*this.layers[i][j][0][3]
+				}
 			}
 		}
 		
